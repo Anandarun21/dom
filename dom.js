33b8bdc1162f2d2b1fd8284bd4539939af98155a
Anandarun21 <anandarun238@gmail.com>
2023-11-19 10:27:05 +0530

    init

---
+++ dom.js
@@ -0,0 +1,19 @@
+// Examione the document objects//
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
+//console.log(document.head);
+//console.log(document.all);
+//console.log(document.all[10]);
+//console.log(document.forms);
+
+// GETelementby id
+
+//console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent = 'Hello';
+//headerTitle.innerText = ' Goodbye'
+header.style.borderBottom = 'solid 3px #000'
