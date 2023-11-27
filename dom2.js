fe334442736db8367f5291a2e155dce21b9508eb
Anandarun21 <anandarun238@gmail.com>
2023-11-27 09:51:54 +0530

    init

--- dom2.js
+++ dom2.js
@@ -70,7 +70,7 @@ var odd = document.querySelectorAll('li:nth-child(odd)');
 var even = document.querySelectorAll('li:nth-child(even)');
 
 for(var i=0; i<odd.length; i++){
-    odd[i].style.backgroundColor = '#f4f4f4';
+    odd[i].style.backgroundColor = 'green';
     even[i].style.backgroundColor = '#ccc';
 }
 
